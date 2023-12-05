7d6be14e94a14e81a36b53d84edb54cad80a63cf
Jo <loluvulol@gmail.com>
2023-12-06 09:32:53 +1000

    feat: attempt day 4

--- days/4/1.ts
+++ days/4/1.ts
@@ -1,8 +1,31 @@
 import getFileContent from "../../helpers/getFileContent";
 
 const runner = async () => {
-  // batter up
   const fileContent = getFileContent(4);
+  const splitByLine = fileContent.split("\n");
+  let total = 0;
+
+  const splitCards = splitByLine.map((line) => line.split("|"));
+
+  const winningNumbers = splitCards.map((cards, i) => cards[0].split(":")[1]);
+
+  const givenNumbers = splitCards.map((cards, i) => cards[1]);
+
+  for (let i = 0; i < winningNumbers.length; i++) {
+    const winners = winningNumbers[i].split(" ").filter((n) => n);
+    winners.map((w, index) => {
+      if (givenNumbers[i].includes(w)) {
+        let iteration = 1;
+        total += iteration;
+        iteration += 1;
+        if (index === winners.length) {
+          iteration = 1;
+        }
+      }
+    });
+  }
+
+  console.log(total);
 };
 
 runner();
