fdff6120896cc85408d20ae4e63d98e0afdff629
Jo <loluvulol@gmail.com>
2023-12-08 12:06:19 +1000

    feat: day 6 complete

--- days/6/2.ts
+++ days/6/2.ts
@@ -2,7 +2,16 @@ import getFileContent from "../../helpers/getFileContent";
 
 const runner = async () => {
   // batter up
-  const fileContent = getFileContent(6);
+  let winningCombos = 0;
+  for (let i = 0; i < 40817772; i++) {
+    let distanceTraveled = 0;
+    distanceTraveled = (40817772 - i) * i;
+
+    if (distanceTraveled > 219101213651089) {
+      winningCombos = winningCombos + 1;
+    }
+  }
+  console.log(winningCombos);
 };
 
 runner();
